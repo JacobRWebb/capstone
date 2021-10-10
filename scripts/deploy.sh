479c7775f95246e9e760e25793d857c75fd52b99
JacobRWebb <xxpwnage37xx@gmail.com>
2021-10-10 14:30:02 -0500

    Updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 ssh -tt root@xodius.io <<-'ENDSSH'
   cd ~/Capstone
   git stash push --include-untracked
-  git pull
+  git pull https://github.com/JacobRWebb/capstone.git
   yarn
   pm2 start
   exit
