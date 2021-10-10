96f3441f5d92c5886b210284243be52eaf97df72
JacobRWebb <xxpwnage37xx@gmail.com>
2021-10-10 17:16:40 -0500

    Changed cookie placeholder and removed completed TODO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,19 @@
 import { serialize } from "cookie";
 import { NextPage } from "next";
-import { useEffect, useState } from "react";
 import NavBar from "../components/navbar";
 import { checkToken } from "../store/authFeature/authFunctions";
 import { wrapper } from "../store/store";
 
 const Index: NextPage = () => {
-  const [reservations, setReservations] = useState<string[]>([]);
-
-  let amount = 100;
-
-  useEffect(() => {
-    let temp: string[] = Array(amount).fill("Testing placeholder");
-    setReservations(temp);
-  }, []);
-
   return (
     <div className="page">
       <NavBar />
       <div className="content">
         <div className="container reservationContainer">
           <h1>Reservations</h1>
-          {reservations.map((value, index) => {
-            return (
-              <div key={index} className="reservation">
+          {/* <div key={index} className="reservation">
                 <p>{value}</p>
-              </div>
-            );
-          })}
+              </div> */}
         </div>
       </div>
     </div>
@@ -37,20 +23,24 @@ const Index: NextPage = () => {
 export const getServerSideProps = wrapper.getServerSideProps(
   (store) =>
     async ({ req, res }) => {
-      //  TODO verify user token with API and propagate user state with response.
       const token = req.cookies.token;
       await store.dispatch(checkToken({ token }));
 
       const state = store.getState();
 
       if (state.Auth.user === null) {
-        res.setHeader(
-          "Set-Cookie",
-          serialize("warning-token", "Placeholder", {
-            path: "/",
-            maxAge: 1,
-          })
-        );
+        if (req.cookies["logged"]) {
+          res.setHeader("Set-Cookie", [
+            serialize("warning-token", "_", {
+              path: "/",
+              maxAge: 1,
+            }),
+            serialize("logged", "_", {
+              path: "/",
+              maxAge: 0,
+            }),
+          ]);
+        }
 
         return {
           redirect: {
@@ -60,6 +50,7 @@ export const getServerSideProps = wrapper.getServerSideProps(
         };
       }
 
+      //  TODO fetch reservations
       return {
         props: {},
       };
