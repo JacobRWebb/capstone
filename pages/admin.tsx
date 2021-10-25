8248e41e5200c5dc70f1de6b6e1fe1b64fdf13b3
JacobRWebb <xxpwnage37xx@gmail.com>
2021-10-24 22:33:08 -0500

    Reset Admin Page

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -5,20 +5,7 @@ const Admin: FunctionComponent = () => {
   return (
     <div className="page">
       <NavBar />
-      <div className="content">
-        <div className="container">
-          <h1>Quick Actions</h1>
-          <span className="separation" />
-          <div className="quickActionMenu">
-            <button className="quickAction">Create Space</button>
-            <button className="quickAction">Manage All Reservations</button>
-          </div>
-        </div>
-        <div className="container">
-          <h1>Manage All Reservations</h1>
-          <span className="separation" />
-        </div>
-      </div>
+      <div className="content"></div>
     </div>
   );
 };
