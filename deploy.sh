0a853d30f07c750ab56c2214dcdc9207d62dd396
JacobRWebb <xxpwnage37xx@gmail.com>
2021-10-30 22:45:00 -0500

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@
 
 ssh -tt root@xodius.io <<-'ENDSSH'
   cd ~/Capstone
+  docker login
   docker stop capstone-frontend
   docker rm capstone-frontend
   docker rmi -f jacobwebb/capstone
   docker pull jacobwebb/capstone:latest
   docker run --name capstone-frontend -p 3000:3000 -d jacobwebb/capstone:latest
+  
   exit
 ENDSSH
